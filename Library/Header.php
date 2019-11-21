a0b21170e10ddd6f7e0500829e15ac3182de7fca
jluciano <jluciano@DTI-GDE-13>
2019-11-21 16:47:04 -0400

    Structure and assets

---
+++ Library/Header.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+<link rel="stylesheet" href="./Library/Assets/css/bootstrap.css"/>
+<link rel="stylesheet" href="./Library/Assets/css/bootstrap.min.css"/>
+<script type="text/javascript" src="./Library/Assets/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="./Library/Assets/js/bootstrap.js"></script> 
+</head>
+
+
+<body>
+<div class="container">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">App Soluciones</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Ver Productos <span class="sr-only"></span></a>
+      <a class="nav-item nav-link" href="#">Ver Clientes</a>
+ 
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 include "./Library/Motor.php";
+include "./Library/Header.php";
+?>
 
+
+<?php
+include "./Library/footer.php";
+
+?>
